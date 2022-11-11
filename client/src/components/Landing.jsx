1bb52a4a0354f412ffab73f38f1d28bfa24d213c
Walter Celiz <wceliz.dev@gmail.com>
2022-11-11 14:30:34 -0300

    landing 0.1

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/landing.css";
 
 export default function Landing() {
     return (
         <div className="landingContainer">
             <div className="landing">
-                <h2 className="landing__h2">Ready To Start</h2>
+                <h2 className="landing__h2">Project <br/>Video Games</h2>
+                <Link className="landing__link" to="/home">
+                    <button className="landing__button">Start !</button>
+                </Link>
             </div>
         </div>
     );
