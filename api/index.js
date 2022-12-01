4121075db6df0c682506ae67236a3214a4493e44
Walter Celiz <wceliz.dev@gmail.com>
2022-12-01 12:33:42 -0300

    stable version 2.0 merged deploy

--- api/index.js
+++ api/index.js
@@ -21,12 +21,13 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { controllerGenres } = require("./src/controllers/genresController");
 const { platformControllers } = require("./src/controllers/platformController");
+const { PORT } = process.env || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   controllerGenres();
   platformControllers();
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Server listening at PORT: ${PORT}`); // eslint-disable-line no-console
   });
 });
