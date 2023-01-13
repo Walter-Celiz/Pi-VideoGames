9eeddd9d8086975e5dfba63c5630e82cfce3aba4
Walter Celiz <wceliz.dev@gmail.com>
2023-01-13 14:56:11 -0300

    for deploy8

--- api/src/db.js
+++ api/src/db.js
@@ -2,26 +2,6 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-<<<<<<< HEAD
-const { DB_DEPLOY } = process.env;
-
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
-//   {
-//     logging: false, // set to console.log to see the raw SQL queries
-//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//   }
-// );
-
-const sequelize = new Sequelize(
-  DB_DEPLOY,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
-
-=======
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
 const sequelize = new Sequelize(DB_DEPLOY, {
@@ -36,7 +16,6 @@ const sequelize = new Sequelize(DB_DEPLOY, {
 //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 //   }
 // );
->>>>>>> deploy
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
