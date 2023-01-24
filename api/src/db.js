9f1591250f4eb767af7afd493b27ba6a4a1fe3dc
Walter Celiz <wceliz.dev@gmail.com>
2023-01-24 12:49:50 -0300

    back deploy

--- api/src/db.js
+++ api/src/db.js
@@ -5,19 +5,20 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY2 } = process.env;
 
-const sequelize = new Sequelize(DB_DEPLOY2, {
-  dialectModule: pg,
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+const sequelize = new Sequelize(
+  DB_DEPLOY2,
+  {
+    dialectModule: pg,
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  }
 
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
-//   {
-//     logging: false, // set to console.log to see the raw SQL queries
-//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//   }
-// );
+  //   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+  //   {
+  //     logging: false, // set to console.log to see the raw SQL queries
+  //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  //   }
+);
 
 const basename = path.basename(__filename);
 
