44e06da2ef6df1ec510a3b10c3afcad5d46c5a9f
Walter Celiz <wceliz.dev@gmail.com>
2023-01-24 14:00:29 -0300

    back w/o deploy

--- api/index.js
+++ api/index.js
@@ -1,20 +1,15 @@
 require("dotenv").config();
 const server = require("./src/app.js");
-const { db } = require("./src/db.js");
+const { conn } = require("./src/db.js");
 const { PORT } = process.env;
 // const { controllerGenres } = require("./src/controllers/genresController");
 // const { platformControllers } = require("./src/controllers/platformController");
 
 // Syncing all the models at once.
-try {
-  db.sync({ force: true }).then(() => {
-    // controllerGenres();
-    // platformControllers();
-    server.listen(PORT, () => {
-      console.log(`#Server listening at Port: ${PORT} 🟢🟢🟢🟢`); // eslint-disable-line no-console
-    });
+conn.sync({ force: true }).then(() => {
+  // controllerGenres();
+  // platformControllers();
+  server.listen(PORT, () => {
+    console.log(`#Server listening at Port: ${PORT}!!! 🟢🟢🟢🟢`); // eslint-disable-line no-console
   });
-} catch (error) {
-  return error;
-  // console.log(error + "#Server not found!!! 🔴🔴🔴🔴");
-}
+});
--- api/src/db.js
+++ api/src/db.js
@@ -1,24 +1,21 @@
 require("dotenv").config();
-const { pg } = require("pg");
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY2 } = process.env;
 
-const sequelize = new Sequelize(
-  DB_DEPLOY2,
-  {
-    dialectModule: pg,
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
+const sequelize = new Sequelize(DB_DEPLOY2, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 
-  //   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
-  //   {
-  //     logging: false, // set to console.log to see the raw SQL queries
-  //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  //   }
-);
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
 
 const basename = path.basename(__filename);
 
@@ -58,5 +55,5 @@ module.exports = {
   Platforms,
   Videogames,
   Genres, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  db: sequelize, // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
