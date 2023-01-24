c7538db30fbc4c8bc764ee77b503386de1830085
Walter Celiz <wceliz.dev@gmail.com>
2023-01-24 14:03:37 -0300

    back w/o deploy

--- api/src/db.js
+++ api/src/db.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
+const { pg } = require("pg");
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY2 } = process.env;
 
 const sequelize = new Sequelize(DB_DEPLOY2, {
+  dialectModule: pg,
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
